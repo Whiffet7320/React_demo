554f8a959c41d5693dabf1920b0d1da4d1f12185
chenyiyu <732073507@qq.com>
2020-12-22 13:48:53 +0800

    demo会移动的球

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Student from './components/Student';
-import StudentList from './components/StudentList'
-const appkey = "demo13_1545210570249";
+import Ball from './components/Ball';
+import BallList from './components/BallList'
 
-async function my_fetch() {
-  let stus = await fetch(`http://api.duyiedu.com/api/student/findAll?appkey=${appkey}`).then(res => {
-    return res.json()
-  }).then(resf => {
-    return resf.data
-  })
-  return stus
-}
-const obj = {
-  address: "重庆111",
-  appkey: "demo13_1545210570249",
-  birth: 1999,
-  ctime: 1607330154,
-  email: "111@qq.com",
-  id: 70949,
-  name: "cyy123456789",
-  phone: "19923621111",
-  sNo: "1111",
-  sex: 0,
-  utime: 1608293101,
-}
-async function render() {
-  ReactDOM.render("数据加载中...", document.getElementById('root'));
-  let stus = await my_fetch();
-  // ReactDOM.render(<Student {...obj} />, document.getElementById('root'));
-  ReactDOM.render(<StudentList stuObj={stus} />, document.getElementById('root'));
-}
-render()
+ReactDOM.render(<BallList />, document.getElementById('root'));
+// ReactDOM.render(<Ball left={100} top={100} xSpeed={500} ySpeed={200} background={'blue'}/>, document.getElementById('root'));
